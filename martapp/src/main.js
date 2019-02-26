b72c4dc215ba050a160698c8b3a9c9c3b631b419
devimalop <devimalop@gmail.com>
2019-02-27 00:16:01 +0100

    Creado proyecto base de Vue CLI + framework7

---
+++ martapp/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
+// Import F7 Styles
+import 'framework7/css/framework7.css';
+
+// Import Icons and App Custom Styles
+import './css/icons.css';
+import './css/app.css';
+
+Framework7.use(Framework7Vue)
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
